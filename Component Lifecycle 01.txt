bc5e0fffdce7ac7a4889f6b82e45a73c1caf1a69
OXIDE <oxidiu777@gmail.com>
2023-05-31 23:27:40 +0300

    done

---
+++ Component Lifecycle 01.txt
@@ -0,0 +1,3 @@
+Component Lifecycle 01
+
+Modify the `Counter` component so that the interval is initialized within the `componentDidMount` life cycle method instead of the constructor. Is the constructor still required?
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -18,4 +18,7 @@ export const Counter = (props) => {
     return <CounterDisplay count={count}/>;
 };
 
+//for this exercise I did nothing because I was using this from the beginning. the useEffect hook has embedded all of the 3 component lifecycle methods:
+// componentDidMount (it runs the code once when the component mounts), componentDidUpdate (it watches the component in the square brackets), and componentWillUnmount (the return function).
+
 
