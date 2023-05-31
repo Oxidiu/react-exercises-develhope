d8cc1f8ec2fe54c60c91539ae3348b3daaa265c0
OXIDE <oxidiu777@gmail.com>
2023-05-31 23:26:16 +0300

    done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,39 +1,21 @@
 import React, { useState, useEffect } from "react";
+import { CounterDisplay } from "./CounterDisplay";
 
 
-export const Counter = () => {
-    const [count, setCount] = useState(0);
+export const Counter = (props) => {
+    const [count, setCount] = useState(props.initialValue);
 
     useEffect(() => {
         const interval = setInterval(() => {
-        setCount(prevCount => prevCount + 1);
-        }, 1000);
+        setCount(prevCount => prevCount + props.pace);
+        }, props.interval);
         // interval()
         return () => {
         clearInterval(interval);
         };
-    }, []);
+    });
 
-    return <h1>{count}</h1>;
+    return <CounterDisplay count={count}/>;
 };
 
-// export class Counter extends React.Component {
-//     state = {
-//         count: 0
-//     }
-//     constructor(props) {
-//         super(props)
-//         setInterval (() => {
-//             this.setState((state) => {
-//                 return {
-//                     count: state.count + 1
-//                 }
-//                 })
-//         }, 1000)
-//     }
-//     render() {
-//         return <div>
-//             <h1>{this.state.count}</h1>
-//         </div>
-//     }
-// }
+
