1a0ccf2aa16b669bd01bf1d44224bddeb49dc0fb
OXIDE <oxidiu777@gmail.com>
2023-05-31 23:21:20 +0300

    done

---
+++ State 03.txt
@@ -0,0 +1,3 @@
+State 03
+
+Modify the `Counter` component so that the initial value of the counter, the increment interval and the increment amount are passed as props to the component.
--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Counter } from './components/Counter';
 function App() {
   return (
     <div className="App">
-      <Counter/>
+      <Counter initialValue={0} pace={2} interval={2000} />
     </div>
   );
 }
