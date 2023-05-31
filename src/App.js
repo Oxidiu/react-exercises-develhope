def2592cd45559a03f1076221d87063993f28e87
OXIDE <oxidiu777@gmail.com>
2023-05-31 23:17:25 +0300

    done

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import { Welcome } from './components/Welcome';
 function App() {
   return (
     <div className="App">
-      <h2>9-conditional-rendering-if-less-than-65</h2>
-      <Welcome name="Mortimer" age={100}/>
+      <Welcome name="Ovi" age={30}/> {/* Age is not displayed */}
+      <Welcome name="John" age={20}/>
     </div>
   );
 }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,14 +4,14 @@ import { Age } from './Age'
 
 export class Welcome extends React.Component{
     static defaultProps = {
-      name: 'Radu'
+        name: 'Radu'
     }
     
     render(){
       return (
         <div>
-          <p> Hello {this.props.name}</p>
-          <div>{this.props.age && <p>{this.props.age > 18 && this.props.age < 65 ?  <Age age={this.props.age}/> : "Please enter a valid age"}</p>}</div>
+            <p> Hello {this.props.name}</p>
+            <div>{this.props.age && this.props.name === "John" && <p>{this.props.age > 18 && this.props.age < 65 ?  <Age age={this.props.age}/> : "Please enter a valid age"}</p>}</div>
         </div>
       )
     }
