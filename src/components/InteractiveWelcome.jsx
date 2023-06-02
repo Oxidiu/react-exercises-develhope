caf0fcb6fb7ecdc449a15dfd3950cb21fb544fca
OXIDE <oxidiu777@gmail.com>
2023-06-02 18:11:09 +0300

    updated

--- src/components/InteractiveWelcome.jsx
+++ src/components/InteractiveWelcome.jsx
@@ -1,16 +1,36 @@
 import React, {useState} from "react"
 import {Welcome} from "./Welcome"
 
+export class InteractiveWelcome extends React.Component {
+    state = {
+        name: ""
 
-export const InteractiveWelcome = () => {
-    const [name, setName] = useState("")
-
-    return (
-        <div>
-            <input onChange={(ev) => {setName(ev.target.value)}}/>
-            <Welcome name={name}/>
-        </div>
-        
-    )
-    
+    }
+    handleInputChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    render() {
+        return (
+            <div>
+                <input type="text" name="name" value="this.state.name" onChange={this.handleInputChange} />
+                <Welcome name={this.state.name}/>
+            </div>
+        )
+    }
 }
+
+
+// export const InteractiveWelcome = () => {
+//     const [name, setName] = useState("")
+
+//     return (
+//         <div>
+//             <input onChange={(ev) => {setName(ev.target.value)}}/>
+//             <Welcome name={name}/>
+//         </div>
+        
+//     )
+    
+// }
