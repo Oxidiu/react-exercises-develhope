35b394bce625f01665ca0b61812e4f26b021aab1
OXIDE <oxidiu777@gmail.com>
2023-06-04 18:33:38 +0300

    added a remove button to every list item

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -18,6 +18,14 @@ export class TodoList extends React.Component {
             items: []
         })
     }
+    
+    removeItem = (i) => {
+        this.setState(() => {
+            const items = [...this.state.items]
+            items.splice(i, 1)
+            return {items}
+        })
+    }
     render() {
         return <div>
             <input type="text" name="newTodo" value={this.state.newTodo} onChange={(event) => {
@@ -26,7 +34,7 @@ export class TodoList extends React.Component {
             <button onClick={this.addItem}>Add</button>
             <button onClick={this.reset}>Reset</button>
             <ul>
-                {this.state.items.map((item) => <li>{item}</li>)}
+                {this.state.items.map((item, i) => <li key={i}>{item} <button key={`button ${i}`} onClick={() => this.removeItem(i)}>Remove</button></li> )}
             </ul>
         </div>
     }
